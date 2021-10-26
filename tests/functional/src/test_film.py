e1a69aca0a4740dac83a92af561b8e1e1ee76994
Sergey Simenshteyn <simenshteyn@gmail.com>
2021-10-26 19:04:32 +0700

    Add some basic tests for films: search, pagination
    Took 28 minutes

--- tests/functional/src/test_film.py
+++ tests/functional/src/test_film.py
@@ -1,5 +1,6 @@
 import pytest
 import aiohttp
+import asyncio
 from elasticsearch import AsyncElasticsearch
 from http import HTTPStatus
 
@@ -16,6 +17,14 @@ class HTTPResponse:
     status: int
 
 
+@pytest.fixture(scope="session")
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
 @pytest.fixture(scope='session')
 async def es_client():
     client = AsyncElasticsearch(hosts=f'{config.es_host}:{config.es_port}')
@@ -30,7 +39,7 @@ async def session():
     await session.close()
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def make_get_request(session):
     async def inner(method: str, params: dict = None) -> HTTPResponse:
         params = params or {}
@@ -55,8 +64,70 @@ def make_get_request(session):
 async def test_film_response_status(make_get_request):
     response = await make_get_request('film/')
     assert response.status == HTTPStatus.OK
+    assert len(response.body) > 0
+
 
 @pytest.mark.asyncio
 async def test_film_sorted_asc_status(make_get_request):
-    response = await make_get_request('/film/?sort=-imdb_rating')
+    response = await make_get_request('film/?sort=imdb_rating')
     assert response.status == HTTPStatus.OK
+    assert len(response.body) > 0
+
+
+@pytest.mark.asyncio
+async def test_film_sorted_desc_status(make_get_request):
+    response = await make_get_request('film/?sort=-imdb_rating')
+    assert response.status == HTTPStatus.OK
+    assert len(response.body) > 0
+
+
+@pytest.mark.asyncio
+async def test_film_page_number(make_get_request):
+    response = await make_get_request('film/?page_number=0')
+    assert response.status == HTTPStatus.OK
+    assert len(response.body) > 0
+
+
+@pytest.mark.asyncio
+async def test_film_page_size(make_get_request):
+    response = await make_get_request('film/?page_size=10')
+    assert response.status == HTTPStatus.OK
+    assert len(response.body) > 0
+
+
+@pytest.mark.asyncio
+async def test_film_page_number_and_size(make_get_request):
+    response = await make_get_request('film/?page_size=10&page_number=0')
+    assert response.status == HTTPStatus.OK
+    assert len(response.body) > 0
+
+
+@pytest.mark.asyncio
+async def test_film_page_number_and_size_sorted_asc(make_get_request):
+    response = await make_get_request('film/?page_size=10&page_number=0'
+                                      '&sort=imdb_rating')
+    assert response.status == HTTPStatus.OK
+    assert len(response.body) > 0
+
+
+@pytest.mark.asyncio
+async def test_film_page_number_and_size_sorted_desc(make_get_request):
+    response = await make_get_request('film/?page_size=10&page_number=0'
+                                      '&sort=-imdb_rating')
+    assert response.status == HTTPStatus.OK
+    assert len(response.body) > 0
+
+
+@pytest.mark.asyncio
+async def test_film_search(make_get_request):
+    response = await make_get_request('film/search/dog')
+    assert response.status == HTTPStatus.OK
+    assert len(response.body) > 0
+
+
+@pytest.mark.asyncio
+@pytest.mark.xfail
+async def test_film_popular_in_genre(make_get_request):
+    response = await make_get_request('film/genre/foo')
+    assert response.status == HTTPStatus.OK
+    assert len(response.body) > 0
